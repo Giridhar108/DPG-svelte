9215125b2421c33c3f3270e4561934d75045e574
German <pentagram777@mail.ru>
2021-06-04 14:00:52 +0500

    first commit

---
+++ src/components/Block.svelte
@@ -0,0 +1,9 @@
+
+<div class={$$props.class}></div>
+
+<style>
+	div {
+    width: 100px;
+    height: 100px;
+  }
+</style>
---
+++ src/components/Container.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Block from "./Block.svelte";
+
+  let current = "";
+</script>
+
+<div class="wrapper">
+  <div
+    class="parent {current === 'red' ? 'active' : ''}"
+    on:click={() => (current = current === 'red' ? '' : 'red')}
+  >
+    <Block class="red" />
+  </div>
+  <div
+    class="parent {current === 'green' ? 'active' : ''}"
+    on:click={() => (current = current === 'green' ? '' : 'green')}
+  >
+    <Block class="green" />
+  </div>
+  <div
+    class="parent {current === 'blue' ? 'active' : ''}"
+    on:click={() => (current = current === 'blue' ? '' : 'blue')}
+  >
+    <Block class="blue" />
+  </div>
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+  }
+  .parent :global(.red) {
+    background-color: red;
+    opacity: 1;
+  }
+  .parent :global(.green) {
+    background-color: green;
+    opacity: 1;
+  }
+  .parent :global(.blue) {
+    background-color: blue;
+    opacity: 1;
+  }
+  .active {
+    opacity: 0;
+  }
+</style>
